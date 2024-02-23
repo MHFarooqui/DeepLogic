d010d8cebc63bad47e766f78f901ae7ac7438712
MHFarooqui <farooquihanif02@gmail.com>
2024-02-23 15:14:27 +0530

    done

--- DeepLogic/urls.py
+++ DeepLogic/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for DeepLogin project.
+URL configuration for DeepLogic project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.0/topics/http/urls/
--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DeepLogin.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DeepLogic.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
